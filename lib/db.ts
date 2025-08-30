577c1baf2d45577d1eb2f384f234e75269d002d5
Rishi attri <rishiattri0004@gmail.com>
2025-08-30 14:26:07 +0530

    prisma integrated

---
+++ lib/db.ts
@@ -0,0 +1,12 @@
+import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/extension";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
